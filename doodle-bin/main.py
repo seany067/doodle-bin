6948d66fb4f1d05749c04be5cf9d3e1371e16119
Sean Escreet <sean@escreet.co.uk>
2022-10-21 13:58:31 +0100

    FastAPI conversion
    * Converted entire project from rust to python fastapi
    * Rust usecase is not really for web frameworks, so python fastapi made more sense
    * HTML page now loads previous doodles

---
+++ doodle-bin/main.py
@@ -0,0 +1,74 @@
+import os
+import uuid
+import boto3
+from typing import Optional
+from botocore.exceptions import ClientError
+from pathlib import Path
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse, Response
+from fastapi.exceptions import HTTPException
+
+# Setup
+
+ROOT_DIR = Path(__file__).parent
+
+with open(ROOT_DIR / "templates/index.html", "r") as f:
+        html_content =  f.read()
+
+class Model(BaseModel):
+    id: Optional[str]
+    content: Optional[str]
+
+#AWS
+
+AWS_SETTINGS = {
+    "aws_access_key_id": os.environ.get("DB_ACCESS_KEY_ID"),
+    "aws_secret_access_key": os.environ.get("DB_SECRET_ACCESS_KEY"),
+    "region_name": os.environ.get("DB_REGION_NAME")
+}
+
+table_name = "doodle-bin"
+
+# FastAPI
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=ROOT_DIR / "static"), name="static")
+
+@app.on_event("startup")
+async def startup():
+    app.state.db = boto3.resource('dynamodb', **AWS_SETTINGS)
+
+# Routes
+
+@app.get("/")
+def index():
+    return RedirectResponse(url=f"/{uuid.uuid4()}")
+
+@app.get("/{id}")
+def index(request: Request, id: uuid.UUID):
+    return HTMLResponse(
+        content=html_content,
+        status_code=200
+    )
+ 
+@app.post("/{id}")
+async def save(request: Request, id: str, body: Model):
+    table = request.app.state.db.Table(table_name)
+    try:
+        res = table.put_item(Item={'id': id, 'content': body.content})
+    except ClientError as e:
+        raise e
+        # raise HTTPException(status_code=404, detail="Doodle could not be saved found")
+    return Response("Ok", status_code=200)
+
+
+@app.get("/load/{id}")
+async def load(request: Request, id: str):
+    try:
+        table = request.app.state.db.Table(table_name)
+        res = table.get_item(Key={'id': id})
+        return res["Item"]
+    except ClientError as e:
+        raise HTTPException(status_code=404, detail="Doodle not found")
