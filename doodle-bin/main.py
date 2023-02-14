8156061a32f5eee2d8e8b68f278ccaf9ef9c5cfa
Sean Escreet <sean@escreet.co.uk>
2023-02-14 12:27:18 +0000

    Removed unused endpoints

--- doodle-bin/main.py
+++ doodle-bin/main.py
@@ -1,7 +1,6 @@
 import os
 import uuid
 import boto3
-from io import BytesIO, StringIO
 from dotenv import dotenv_values
 from typing import Optional
 from botocore.exceptions import ClientError
@@ -12,8 +11,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse, Response, StreamingResponse
 from fastapi.exceptions import HTTPException
 from fastapi.templating import Jinja2Templates
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM, renderPDF
 
 # Setup
 
@@ -72,46 +69,6 @@ def widge_index(request: Request, id: uuid.UUID):
         raise HTTPException(status_code=404, detail="Doodle not found")
     img_data: str = res["Item"]["image"]
     return Response(img_data, status_code=200, headers={"Content-type": "image/svg+xml"}, media_type="image/svg+xml")
-
-@app.get("/png/{id}")
-def widge_index(request: Request, id: uuid.UUID):
-    try:
-        table = request.app.state.db.Table(table_name)
-        res = table.get_item(Key={'id': str(id)})
-    except:
-        raise HTTPException(status_code=404, detail="Doodle not found")
-
-    if res is None or 'image' not in res["Item"]:
-        raise HTTPException(status_code=404, detail="Doodle not found")
-    img_data: str = res["Item"]["image"]
-    img_io = StringIO()
-    img_io.write(img_data)
-    img_io.seek(0)
-    render_obj = svg2rlg(img_io)
-    out_io = BytesIO()
-    renderPM.drawToFile(render_obj, out_io, fmt="PNG")
-    out_io.seek(0)
-    return StreamingResponse(out_io, status_code=200, headers={"Content-type": "image/png"}, media_type="image/png")
-
-@app.get("/pdf/{id}")
-def widge_index(request: Request, id: uuid.UUID):
-    try:
-        table = request.app.state.db.Table(table_name)
-        res = table.get_item(Key={'id': str(id)})
-    except:
-        raise HTTPException(status_code=404, detail="Doodle not found")
-
-    if res is None or 'image' not in res["Item"]:
-        raise HTTPException(status_code=404, detail="Doodle not found")
-    img_data: str = res["Item"]["image"]
-    img_io = StringIO()
-    img_io.write(img_data)
-    img_io.seek(0)
-    render_obj = svg2rlg(img_io)
-    out_io = BytesIO()
-    renderPDF.drawToFile(render_obj, out_io)
-    out_io.seek(0)
-    return StreamingResponse(out_io, status_code=200, headers={"Content-type": "application/pdf"}, media_type="application/pdf")
 
 
 @app.post("/{id}")
